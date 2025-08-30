4569ac67cd37bcd66337a4cfe99b3724323f08f1
varun <varunkumar2004.vk@gmail.com>
2025-08-30 22:10:03 +0530

    search feature implemented

--- base/views.py
+++ base/views.py
@@ -1,11 +1,26 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
-from .models import Room
+from django.db.models import Q
+from .models import Room, Topic
 from .forms import RoomForm
 
 def home(request):
-    rooms = Room.objects.all()
-    return render(request=request, template_name="base/home.html", context={"rooms": rooms})
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    # Q -> used for complex queries with and or conditions
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q) |
+        Q(name__icontains=q) | 
+        Q(description__icontains=q) 
+    )
+
+    topics = Topic.objects.all() 
+
+    context= {
+        "rooms": rooms, 
+        "topics": topics,
+        "room_count": rooms.count()
+    }
+
+    return render(request=request, template_name="base/home.html", context=context)
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
